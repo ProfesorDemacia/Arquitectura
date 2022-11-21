e02b29d4c4758f8efd7108040cf30383688bfad2
Cesar Contreras <90733085+ProfesorDemacia@users.noreply.github.com>
2022-11-21 10:15:25 -0600

    Add files via upload

---
+++ Clifford/src/clifford/Clifford.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clifford;
+
+import CapaGUI.VistaMenu;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Clifford {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       VistaMenu pMenu = new VistaMenu();
+       pMenu.setVisible(true);
+    }
+    
+}
